d230fcfb636cd9f0e57c4ac989dc0c844a28b0ec
本间白猫 <139044047+honmashironeko@users.noreply.github.com>
2024-05-20 22:49:28 +0800

    Add files via upload
    一键部署脚本

---
+++ setup_docker.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# 显示作者信息
+echo "docker镜像作者：本间白猫"
+echo "公众号：樱花庄的本间白猫"
+echo "博客：https://y.shironekosan.cn"
+
+# 安装Docker
+echo "正在安装Docker..."
+yum -y install docker
+
+# 启动Docker服务
+echo "正在启动Docker服务..."
+systemctl start docker
+
+# 拉取Docker镜像
+echo "正在拉取Docker镜像..."
+docker pull honmashironeko/arl-docker
+
+# 运行Docker容器
+echo "正在运行Docker容器..."
+docker run -d -p 5003:5003 --name arl --privileged=true honmashironeko/arl-docker /usr/sbin/init
