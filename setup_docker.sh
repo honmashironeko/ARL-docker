07af1a9543a2305715ab1207aca0483f34c45336
本间白猫 <139044047+honmashironeko@users.noreply.github.com>
2024-05-29 15:37:32 +0800

    Update setup_docker.sh

--- setup_docker.sh
+++ setup_docker.sh
@@ -12,9 +12,8 @@ systemctl start docker
 
 echo "请选择要安装的版本："
 echo "1) arl-docker-initial：ARL初始版本，仅去除域名限制。"
-echo "2) arl-docker-portion：ARL部分指纹版本，去除域名限制，并增加 5232 条指纹。"
-echo "3) arl-docker-all：ARL完全指纹版本，去除域名限制，全量 6990 条指纹。"
-read -p "请输入选项（1-3）：" version_choice
+echo "2) arl-docker-all：ARL完全指纹版本，去除域名限制，全量 7165 条指纹。"
+read -p "请输入选项（1-2）：" version_choice
 
 case $version_choice in
     1)
@@ -24,12 +23,6 @@ case $version_choice in
         docker run -d -p 5003:5003 --name arl --privileged=true honmashironeko/arl-docker-initial /usr/sbin/init
         ;;
     2)
-        echo "正在拉取 Docker 镜像：arl-docker-portion..."
-        docker pull honmashironeko/arl-docker-portion
-        echo "正在运行 Docker 容器..."
-        docker run -d -p 5003:5003 --name arl --privileged=true honmashironeko/arl-docker-portion /usr/sbin/init
-        ;;
-    3)
         echo "正在拉取 Docker 镜像：arl-docker-all..."
         docker pull honmashironeko/arl-docker-all
         echo "正在运行 Docker 容器..."
